cf85d15feba1e83fa1e4d5a661f196dfc2760c72
Ben Tandy <ben@afriendcalledben.com>
2026-03-19 17:01:07 +0000

    Fix hero bg color and remove baseline gap

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 const Hero: React.FC = () => {
   return (
-    <div className="w-full flex justify-center" style={{ backgroundColor: '#C9FF00' }}>
+    <div className="w-full flex justify-center leading-[0]" style={{ backgroundColor: '#C3F000' }}>
       <img
         src="/hero.jpg"
         alt="The place for props with personality!"
